d72a997caa1957fc0750222bc841b06eb5520649
rohanvron <164373071+rohanvron@users.noreply.github.com>
2024-09-05 22:35:18 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,12 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
+app.use(cors({
+    origin: 'https://rohanvron.github.io',
+    credentials: true,
+  }));
+  
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
